f97d95371acf4a68ddd61454a08135b1d4014fb2
zhuyu <1359231385@qq.com>
2016-06-03 23:47:43 +0800

    使用scrapy抓取逗游主页想要的信息

---
+++ GameInfo/items.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+class GameInfoItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field
+    # 标题 url 二级url
+    hotTitle=scrapy.Field()
+    url=scrapy.Field()
+    pass
---
+++ GameInfo/spiders/doyo.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.spiders import Spider
+from scrapy.selector import Selector
+from GameInfo.items import GameInfoItem
+class DoyoSpider(scrapy.Spider):
+    name = "doyo"
+    allowed_domains = ["doyo.cn"]
+    start_urls = [
+       'http://www.doyo.cn/',
+    ]
+    def parse(self, response):
+        """
+        The lines below is a spider contract. For more info see:
+        http://doc.scrapy.org/en/latest/topics/contracts.html
+
+        @url http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/
+        @scrapes name
+        """
+        sel = Selector(response)
+        sites = sel.xpath('//div[@class="tl"]')
+        items = []
+
+        for site in sites:
+            item = GameInfoItem()
+            item['hotTitle'] = site.xpath('a/text()').extract()
+            item['url'] = site.xpath('a/@href').extract()
+           # item['description'] = site.xpath('text()').re('-\s[^\n]*\\r')
+            items.append(item)
+
+        return items
